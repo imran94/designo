6c4dea4b5f38d0357da98d8d9a7525bacf1dd721
imran94 <iraja094@gmail.com>
2024-08-17 05:03:13 +0500

    Change animation behavior to run only once on visible

--- js/anims.js
+++ js/anims.js
@@ -1,37 +1,21 @@
 const startAnimation = (entries, observer) => {
   entries.forEach((entry) => {
-    entry.target.classList.toggle("animate", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      if (!entry.target.classList.contains("animate")) {
+        entry.target.classList.add("animate");
+      }
+      observer.unobserve(entry.target);
+    }
   });
 };
 
 const observer = new IntersectionObserver(startAnimation);
 const options = { root: null, rootMargin: "0px", threshold: 1.0 };
-const observeNullRoot = (el) => {
-  if (el.className) {
-    observer.observe(el, options);
-  }
-};
-const observeChildren = (el) => {
-  el.childNodes.forEach(observeNullRoot);
-};
 
-document.querySelectorAll(".banner").forEach((b) => {
-  observeNullRoot(b);
-  observeChildren(b);
-});
-document.querySelectorAll(".banner-secondary").forEach((b) => {
-  observeNullRoot(b);
-  observeChildren(b);
-});
-document.querySelectorAll(".design-type").forEach(observeNullRoot);
-document.querySelectorAll(".skill").forEach(observeChildren);
-document.querySelectorAll(".location").forEach(observeChildren);
-document.querySelectorAll(".map").forEach(observeNullRoot);
-document.querySelectorAll(".project").forEach((p) => {
-  observeNullRoot(p);
-  observeChildren(p);
-});
-document.querySelectorAll(".project-contact-section").forEach((p) => {
-  observeNullRoot(p);
-  observeChildren(p);
-});
+document
+  .querySelectorAll("main *")
+  .forEach((el) => observer.observe(el, options));
+
+document
+  .querySelectorAll("footer *")
+  .forEach((el) => observer.observe(el, options));
